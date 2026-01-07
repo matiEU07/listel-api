5005cb1d00cccd879160c2268e3c06e196af7741
Jakub Olejnik <jacobole2000@gmail.com>
2026-01-07 21:53:35 +0100

    Added queue for pop3 requests

--- src/pop3.ts
+++ src/pop3.ts
@@ -2,28 +2,31 @@ import Pop3Command from "node-pop3";
 
 import { ParsedMail, simpleParser } from "mailparser";
 
+import PQueue from "p-queue"
+
+const popQueue = new PQueue({ concurrency: 5 })
+
 export const pop3_getPaginatedMails = async (pagination: number, page: number = 1): Promise<ParsedMail[]> => {
-    const pop3 = new Pop3Command({
-        user: "jakubolejnik@tenco.waw.pl",
-        password: "adminadmin",
-        host: "mail.monika286.mikr.dev",
-        tls: true
+    return popQueue.add(async () => {
+        const pop3 = new Pop3Command({
+            user: "jakubolejnik@tenco.waw.pl",
+            password: "adminadmin",
+            host: "mail.monika286.mikr.dev",
+            tls: true
+        })
+
+        const [mailsNumStr] = (await pop3.STAT()).split(" ")
+        const mailsNum = parseInt(mailsNumStr)
+
+        const mails: ParsedMail[] = []
+
+        for (let i = mailsNum - (pagination * (page - 1)); i > 0 && i > mailsNum - (pagination * page); i--) {
+            mails.push(await readMessage(pop3, i))
+        }
+
+        await pop3.QUIT()
+        return mails
     })
-
-    const [mailsNumStr, _] = (await pop3.STAT()).split(" ")
-
-    const mailsNum = parseInt(mailsNumStr)
-
-    const mails: ParsedMail[] = [];
-
-    for (let i = mailsNum - (pagination * (page - 1)); i > 0 && i > mailsNum - (pagination * page); i--) {
-        mails.push(await readMessage(pop3, i))
-    }
-
-
-
-
-    return mails;
 }
 
 
